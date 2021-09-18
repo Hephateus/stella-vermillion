70cdbb5236b1aaabb861434af33a6d07fbc0493a
Sountren <70284886+Hephateus@users.noreply.github.com>
2021-09-18 19:50:12 +0700

    Create serverinfo.cmd

---
+++ src/Discord-command/Tools/serverinfo.cmd
@@ -0,0 +1,8 @@
+$nomention
+$title[Server Info]
+$description[Server name: $serverName[$guildID]
+Server region: $serverRegion
+Server owner: $username[$serverOwner] ($serverOwner)
+Server verification level: $serverVerificationLvl
+Server total member: $membersCount]
+$serverIcon
